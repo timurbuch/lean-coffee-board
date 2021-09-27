3316a4e7a1ca988f3ab6f8f6d8ea2c00592249a9
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-27 14:26:07 +0100

    Refactor server to use express

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http')
-const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('<h1>Hello, Backend!</h1>')
+app.get('/', (req, res) => {
+  res.send('Hello, Backend!')
 })
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server läuft auf Port ${port}`)
 })
