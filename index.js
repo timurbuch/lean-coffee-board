f407ed98f0c6444d34e1fc530a021861349ac1b7
Timur Buchberger <timo.buchberger@gmail.com>
2021-09-27 14:24:49 +0100

    add express.js to project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello Backend");
+const http = require('http')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.end('<h1>Hello, Backend!</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`)
+})
